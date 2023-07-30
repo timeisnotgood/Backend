1b2942d17342973947eb23a1bb8cbf2cd73d8be7
timeisnotgood <bhoopathicl@gmail.com>
2023-07-30 15:38:35 +0530

    prac

--- serv.js
+++ serv.js
@@ -1,80 +1,18 @@
 const express = require("express")
-const app = express()
-const news = require("./practice/model/newmodel")
 const dotenv = require("dotenv").config()
-const bcrypt = require('bcrypt')
 const mongoose = require("mongoose")
-const jwt = require("jsonwebtoken")
-const { authschema } = require("./middleware/authschema")
 
+const app = express()
+const port = process.env.PORT || 5000
 
-app.use(express.json())
-
-app.get('/user/get', async(req, res) =>{
-    res.status(200).send("Its working")
-})
-
-
-
-app.post('/user/register', async(req, res)=>{
-    const {username, password} = req.body
-
-    if(!username || !password){
-        res.status(400).send("All Fields are Empty")
+try {
+    const connect = mongoose.connect("mongodb+srv://bhoopathi:bhoopathi@cluster0.gfpy8oi.mongodb.net/bhoopathi?retryWrites=true&w=majority")
+    if(connect){
+        app.listen(port, (req, res) =>{
+            console.log( " server is Started in port : ", port);
+        })
     }
-
-    try {
-        const valid = await authschema.validateAsync( req.body )
-        console.log(valid);
-    } catch (error) {
-        console.log(error);
-    }
-
-    const hashedpassword = await bcrypt.hash(password, 10)
-
-    const data = await news.create({
-        username,
-        password:hashedpassword
-    })
-
-    if(data){
-        res.status(200).json(data)
-    }
-
-})
-
-
-
-app.post('/user/login', async(req,res)=>{
-
-    const {username, password} = req.body
-    const user = await news.findOne({username})
-    console.log(user);
-    
-    if(user){
-        const accesstoken = jwt.sign({
-            use : {
-                username : user.username,
-                password : user.password
-            }
-        },process.env.ACCESS_TOKEN_SECRET,{expiresIn : "10m"})
-        res.status(200).json({accesstoken})
-    }
-})
-
-
-mongoose.connect("mongodb+srv://bhoopathi:bhoopathi@cluster0.gfpy8oi.mongodb.net/bhoopathi?retryWrites=true&w=majority")
-.then(()=>{
-    console.log(mongoose.connection.host, mongoose.connection.name);
-})
-.catch((err)=>{
-    console.log(err.message);
-})
-
-
-app.listen(5000, (req, res)=>console.log("Server started"))
-
-
-
-
-
+    console.log(connect.connection.host, connect.connection.name);
+} catch (error) {
+    console.log(error);
+}
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const errors = require("./middleware/errorHandler")
 const connectdb = require("./config/dbconnect")
 const dotenv = require("dotenv").config()
+const fs = require('node:fs');
+const path = require("node:path");
 
 connectdb()
 const app = express()
@@ -9,6 +11,17 @@ const port = process.env.PORT || 5000
 
 app.use(errors)
 app.use(express.json())
+app.use('^/$', (req, res, next) =>{
+    fs.readFile(path.resolve('./assets/index.html'), 'utf-8', (err, data)=>{
+        if(err){
+            console.log(err);
+            return res.status(500).send("Something went Wrong")
+        }
+        return res.send
+    })
+})
+
+
 app.use('/api/contacts', require("./routes/contactsroute"))
 app.use('/api/users', require("./routes/usersroute"))
 // app.use('/api/us', require("./routes/us"))
